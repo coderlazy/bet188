ff3bc02274fc5e9b286fc5199df43cd3d0c40ad9
coderlazy <cuongbui.203@gmail.com>
2016-09-18 00:10:48 +0700

    run

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -50,7 +50,6 @@ class HomeController extends Controller {
     public function saveData() {
         $k = json_decode(crawlData());
         $all_match = $k->mod->d[0]->c;
-		dd($k->mod);
         foreach ($all_match as $match) {
 			
             $match_id = $this->createMatch($match->e[0]);
